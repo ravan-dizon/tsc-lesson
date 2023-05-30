1a9b3fbf90b7f1ff71da310f416e723c8e82b243
John Ravan A. Dizon <johnravand@gmail.com>
2023-05-30 22:35:14 +0800

    commit basic ts

---
+++ sanbox.js
@@ -0,0 +1,8 @@
+var character = 'Ravan';
+var age = 22;
+var isHandsome = true;
+age = 18;
+var circle = function (diametr) {
+    return diametr * Math.PI;
+};
+console.log(circle(20));
---
+++ sanbox.ts
@@ -0,0 +1,10 @@
+let character = 'Ravan';
+let age = 22;
+let isHandsome = true;
+
+age = 18;
+
+const circle = (diametr : number) => {
+    return diametr * Math.PI;
+}
+console.log(circle(20));
