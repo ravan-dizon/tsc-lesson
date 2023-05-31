d210d848871270eeb0d7c1374e112dba9443de7e
John Ravan A. Dizon <johnravand@gmail.com>
2023-05-31 20:46:08 +0800

    commiting any types

--- sanbox.ts
+++ sanbox.ts
@@ -1,47 +1,28 @@
-// explicit types
-let char : string;
-let age : number;
-let isOn : boolean;
+// any type
 
-// age = '2';
-age = 22;
+let age: any;
 
-// isOn = 'TRUE';
-isOn = true;
+age = true;
+console.log(age);
 
-// arrays
-// let ninjas : string[]; 
+age = 'hello';
+console.log(age);
 
-let ninjas : string[] = [];// ->always initialize empty array
+age = { name: 'superV'};
+console.log(age);
 
-ninjas.push('john');
+let mixed: any[] = [];
 
-// union types -> if there is a mixed data type inside your array
-
-let mixed : (string|number|boolean)[] =[]; // -> use parenthesis when you are using union in array
-
-mixed.push('lazy');
 mixed.push(34);
+mixed.push('hello');
 mixed.push(true);
-mixed.push(34);
-
 console.log(mixed);
 
-// -> we can also use union type in a simple/normal variable not only in array
-let uid :string|number; //you can only used parenthesis when you are using array
+let ninjaAny : {name : any, age: any};
 
-uid = '1234';
-uid = 1234;
-// uid = false;
+ninjaAny = {name: 'ivy', age:'23'};
+console.log(ninjaAny);
 
-//Object
-let nijaObj : object;
+ninjaAny = {name: true , age:34};
+console.log(ninjaAny);
 
-nijaObj = {name : 'cong', age: 30};
-
-let nijaObj2: {
-    name: string,
-    age: number,
-    isOff: boolean
-};
-nijaObj2 = {name: 'luigi', age: 23, isOff: true};
