59ecf70a55d86ceb0e23f91c521404dae42fcb04
John Ravan A. Dizon <johnravand@gmail.com>
2023-06-03 21:04:58 +0800

    interface

--- public/app.js
+++ public/app.js
@@ -1,11 +1,17 @@
-import { invoice } from './classes/invoice.js';
-const invOne = new invoice('mario', 'work on the mario website', 1000);
-const invTwo = new invoice('luigi', 'work on the luigi website', 1500);
-// console.log(invOne, invTwo);
-let invoices = [];
-invoices.push(invOne);
-invoices.push(invTwo);
-// console.log(invoices);
-invoices.forEach(inv => {
-    console.log(inv.format());
-});
+"use strict";
+const me = {
+    name: 'John Ravan',
+    age: 22,
+    speak(text) {
+        console.log(text);
+    },
+    spend(amount) {
+        console.log('I spent', amount);
+        return amount;
+    }
+};
+const greetPerson = (person) => {
+    console.log('hello', person.name);
+};
+greetPerson(me);
+console.log(me);
--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,24 @@
-import { invoice } from './classes/invoice.js'
+//interfaces
+interface isPerson{
+    name: string;
+    age: number;
+    speak(a: string) : void;
+    spend(a: number) : number;
+}
+const me : isPerson ={
+    name: 'John Ravan',
+    age: 22,
+    speak(text: string) : void{
+        console.log(text);
+    },
+    spend(amount: number): number{
+        console.log('I spent', amount);
+        return amount;
+    }
+}
+const greetPerson = (person: isPerson) => {
+    console.log('hello', person.name)
+}
+greetPerson(me);
+console.log(me);
 
-const invOne = new invoice('mario', 'work on the mario website', 1000);
-const invTwo = new invoice('luigi', 'work on the luigi website', 1500);
-
-// console.log(invOne, invTwo);
-
-let invoices: invoice[] = [];
-invoices.push(invOne);
-invoices.push(invTwo);
-
-// console.log(invoices);
-
-invoices.forEach(inv => {
-    console.log(inv.format());
-});
